e8a5e703070db3b4c2849bcc69c8981f399bb8ec
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-02 22:29:33 +0530

    [sailesh] Add: DailyEmpWage

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace EmployeWageComputation
     class Program
     {
         static void Main(string[] args)
-        {
+        {  
+            //Constants
             int IS_FULL_TIME = 1;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
@@ -21,6 +22,20 @@ namespace EmployeWageComputation
             {
                 Console.WriteLine("Employee is absent");
             }
+            
+            
+            int EMP_RATE_PER_HOUR = 20;
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            //else
+            //empHrs = 0;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Daily Emp Wage: " + empWage);
             Console.ReadKey();
         }
     }
