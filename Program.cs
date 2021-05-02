6cd4635468a50916372986c786f2685b257c6327
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-02 22:35:53 +0530

    [sailesh] Add: PartTimeWage

--- Program.cs
+++ Program.cs
@@ -9,33 +9,24 @@ namespace EmployeWageComputation
     class Program
     {
         static void Main(string[] args)
-        {  
+        {
             //Constants
-            int IS_FULL_TIME = 1;
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
-            {
-                Console.WriteLine("Employee is present");
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-            }
-            
-            
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             //variables
             int empHrs = 0;
             int empWage = 0;
-            if (empCheck == IS_FULL_TIME)
-            {
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+                empHrs = 4;
+            else if (empCheck == IS_FULL_TIME)
                 empHrs = 8;
-            }
             //else
             //empHrs = 0;
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Daily Emp Wage: " + empWage);
+            Console.WriteLine("Emp Wage: " + empWage);
             Console.ReadKey();
         }
     }
