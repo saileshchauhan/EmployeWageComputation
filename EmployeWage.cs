cda5c19210f3c7ccd1dcb72075dd0683f9c3149a
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-03 09:13:39 +0530

    [sailesh] Refactor: Using Switch Case

--- EmployeWage.cs
+++ EmployeWage.cs
@@ -6,25 +6,30 @@ using System.Threading.Tasks;
 
 namespace EmployeWageComputation
 {
-    class Program
+    class EmployeWage
     {
         static void Main(string[] args)
         {
             //Constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             //variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
-                empHrs = 4;
-            else if (empCheck == IS_FULL_TIME)
-                empHrs = 8;
-            //else
-            //empHrs = 0;
+            switch (empCheck)
+            {
+                case 1:
+                    empHrs = 4;
+                    break;
+                case 2:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+
+            }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage: " + empWage);
             Console.ReadKey();
