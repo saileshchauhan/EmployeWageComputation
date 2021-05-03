ebb7d002e57ccd5a8544fa361a21414356054a5a
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-03 09:18:42 +0530

    [sailesh] Add: Wages for 20 days Month

--- EmployeWage.cs
+++ EmployeWage.cs
@@ -12,26 +12,32 @@ namespace EmployeWageComputation
         {
             //Constants
             int EMP_RATE_PER_HOUR = 20;
+            int WORKING_DAY_PER_MONTH = 20;
             //variables
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int totalEmpHrs = 0;
+            for (int day = 1; day <= WORKING_DAY_PER_MONTH; day++)
             {
-                case 1:
-                    empHrs = 4;
-                    break;
-                case 2:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case 1:
+                        empHrs = 4;
+                        break;
+                    case 2:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
 
+                }
+                totalEmpHrs += empHrs;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage: " + empWage);
+            Console.WriteLine("Total Emp Wage: " + empWage);
             Console.ReadKey();
         }
     }
