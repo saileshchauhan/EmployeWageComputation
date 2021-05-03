7a0cbe4582edc526724f786399c9cfc6697cae24
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-03 09:27:04 +0530

    [sailesh] Add: Conditional While use

--- EmployeWage.cs
+++ EmployeWage.cs
@@ -17,16 +17,19 @@ namespace EmployeWageComputation
             int empHrs = 0;
             int empWage = 0;
             int totalEmpHrs = 0;
-            for (int day = 1; day <= WORKING_DAY_PER_MONTH; day++)
+            int day = 0;
+            while (day <= WORKING_DAY_PER_MONTH && totalEmpHrs <= 100)
             {
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
                 {
                     case 1:
+                        day++;
                         empHrs = 4;
                         break;
                     case 2:
+                        day++;
                         empHrs = 8;
                         break;
                     default:
@@ -35,8 +38,10 @@ namespace EmployeWageComputation
 
                 }
                 totalEmpHrs += empHrs;
+                empWage = empHrs * EMP_RATE_PER_HOUR;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("ToTal work days " + day);
+            Console.WriteLine("ToTal work Hours " + totalEmpHrs);
             Console.WriteLine("Total Emp Wage: " + empWage);
             Console.ReadKey();
         }
