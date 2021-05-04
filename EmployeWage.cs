12dd0a18abd9fec7b07e5d9da388f4f51f4b7029
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-04 10:16:24 +0530

    [sailesh] Add: parameters to method

--- EmployeWage.cs
+++ EmployeWage.cs
@@ -10,22 +10,19 @@ namespace EmployeWageComputation
     {
         static void Main(string[] args)
         {
-            ComputeEmpWage();
+            ComputeEmpWage("Reliance",6,20,100);
             Console.ReadKey();
         }
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HRS_IN_MONTH = 10;
-        public static int ComputeEmpWage()
+        public static int ComputeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
 
             //variables
             int empHrs = 0;
             int totalEmpHrs = 0;
             int totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -45,8 +42,8 @@ namespace EmployeWageComputation
                 totalEmpHrs += empHrs;
                 Console.WriteLine("Days#: " + totalWorkingDays + " Emp Hrs : " + empHrs);
             }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company: " + company + " is : " + totalEmpWage);
             return totalEmpWage;
         }
     }
